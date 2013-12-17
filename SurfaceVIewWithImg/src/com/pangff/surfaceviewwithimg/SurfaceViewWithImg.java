fd522b456449b7c2ea4735dc1df220a5f56e671c
pangff <pangff@gmail.com>
2013-12-17 22:08:08 +0800

    改线程为Run loop

--- SurfaceVIewWithImg/src/com/pangff/surfaceviewwithimg/SurfaceViewWithImg.java
+++ SurfaceVIewWithImg/src/com/pangff/surfaceviewwithimg/SurfaceViewWithImg.java
@@ -6,18 +6,20 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class SurfaceViewWithImg extends SurfaceView implements SurfaceHolder.Callback {
 
-  private TutorialThread _thread;
+  LooperThread _looperThread;
 
   public SurfaceViewWithImg(Context context) {
     super(context);
     getHolder().addCallback(this);
-    _thread = new TutorialThread(getHolder(), this);
+    _looperThread = new LooperThread(getHolder(), this);
   }
 
   @Override
@@ -26,58 +28,65 @@ public class SurfaceViewWithImg extends SurfaceView implements SurfaceHolder.Cal
     Bitmap _scratch = BitmapFactory.decodeResource(getResources(), R.drawable.sina);
     canvas.drawColor(Color.BLACK);
     canvas.drawBitmap(_scratch, 10, 10, null);
-    _thread.setRunning(false);
   }
 
   public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {}
 
   public void surfaceCreated(SurfaceHolder arg0) {
-    _thread.setRunning(true);
-    _thread.start();
+    _looperThread.start();
+    new Handler().postDelayed(new Runnable() {
+
+      @Override
+      public void run() {
+        _looperThread.mHandler.sendMessage(Message.obtain());
+      }
+    }, 500);
   }
 
   public void surfaceDestroyed(SurfaceHolder arg0) {
-    boolean retry = true;
-    _thread.setRunning(false);
-    while (retry) {
-      try {
-        _thread.join();
-        retry = false;
-      } catch (InterruptedException e) {}
-    }
+
+    _looperThread.mHandler.post(new Runnable() {
+      @Override
+      public void run() {
+        Looper.myLooper().quit();
+      }
+    });
+
   }
 
-  class TutorialThread extends Thread {
+  class LooperThread extends Thread {
+    public Handler mHandler;
     private SurfaceHolder _surfaceHolder;
     private SurfaceViewWithImg _panel;
-    private boolean _run = false;
+    Canvas c;
 
-    public TutorialThread(SurfaceHolder surfaceHolder, SurfaceViewWithImg panel) {
+    public LooperThread(SurfaceHolder surfaceHolder, SurfaceViewWithImg panel) {
       _surfaceHolder = surfaceHolder;
       _panel = panel;
     }
 
-    public void setRunning(boolean run) {
-      _run = run;
-    }
-
-    @SuppressLint("WrongCall")
     @Override
     public void run() {
-      Canvas c;
-      while (_run) {
-        c = null;
-        try {
-          c = _surfaceHolder.lockCanvas(null);
-          synchronized (_surfaceHolder) {
-            _panel.onDraw(c);
-          }
-        } finally {
-          if (c != null) {
-            _surfaceHolder.unlockCanvasAndPost(c);
+      Looper.prepare();
+      mHandler = new Handler() {
+        @SuppressLint("WrongCall")
+        @Override
+        public void handleMessage(Message msg) {
+          c = null;
+          try {
+            c = _surfaceHolder.lockCanvas(null);
+            synchronized (_surfaceHolder) {
+              _panel.onDraw(c);
+            }
+          } finally {
+            if (c != null) {
+              _surfaceHolder.unlockCanvasAndPost(c);
+            }
           }
         }
-      }
+      };
+
+      Looper.loop();
     }
   }
 }
